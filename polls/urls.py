3ff9572c92a1285cedf8b9d7f8473ab39725f699
keinblue <keinblue@naver.com>
2018-03-25 10:42:44 +0900

    yyy

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post/', views.post, name='post'),
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, Menuda")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def post(request):
+    if request.method == "POST":
+         return HttpResponse("POST, Menuda")
+    else:
+        return HttpResponse("GET, Menuda")
