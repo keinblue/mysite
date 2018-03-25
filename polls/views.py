30cde21a063bfeaee5fd3f2fbeb201c3b049a7cf
keinblue <keinblue@naver.com>
2018-03-25 23:12:41 +0900

    aa

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
@@ -7,10 +8,14 @@ def index(request):
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
+@csrf_exempt
 def post(request):
 	if request.method == "POST":
-		msg = request.POST['choice']
-		return HttpResponse("POST, Menuda %s" % msg)
+		LoginSNS = request.POST['LoginSNS']
+		LoginID = request.POST['LoginID']
+		LoginPW = request.POST['LoginPW']
+		Msg = "LoginSNS:%s\nLoginID:%s\nLoginPW:%s" % (LoginSNS, LoginID, LoginPW)
+		return HttpResponse("POST, Menuda\n%s" % Msg)
 	else :
 		return HttpResponse("GET, Menuda")
 #    if request.method == "POST":
